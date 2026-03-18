115d250480e9e7a9a0d5451b9fa13e56de5f6125
putajiri22 <putajiri22@spseplzen.cz>
2026-03-18 17:08:51 +0100

    metody pro flagging a reveal

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -12,4 +12,16 @@ public class Cell
             return "M";
         return adjacentMines.ToString();
     }
+
+    //v podstatě ToString pro výpis buňky tak jak je ve hře, neboli jestli je skrytá apod.
+    public string ToStringForGame()
+    {
+        if(isFlagged)
+            return "F";
+        if(!isRevealed) 
+            return "H";
+        if(hasMine)
+            return "M";
+        return adjacentMines.ToString();
+    }
 }
